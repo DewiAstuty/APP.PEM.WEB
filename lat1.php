43f5ca155ab832da0c60f163d951f48e49af7d9b
DewiAstuty_ <79793340+DewiAstuty@users.noreply.github.com>
2021-03-06 22:44:15 +0700

    Add files via upload
    Nama : Dewi Astuty
    NPM : 888740306810029
    Kelas : KA-P.18.1K
    Mata Kuliah : Pemrograman Aplikasi Web

---
+++ lat1.php
@@ -0,0 +1,6 @@
+<?php 
+	echo "Hello <br>";
+	echo "Nama saya Dewi Astuty <br>";
+	echo "Saya Kuliah di UNIVERSITAS STEKOM KENDAL <br>";
+	echo "Ayo kita belajar PHP bersama";
+ ?>
---
+++ variabel.php
@@ -0,0 +1,11 @@
+<?php 
+// sekarang kita belajar variabel
+$namadepan ="Dewi " ;
+$namabelakang = "Astuty" ;
+$jumlahbarang = 6 ;
+$harga = 120000;
+
+echo "Nama saya adalah $namadepan $namabelakang <br>";
+echo "Jumlah Barang : $jumlahbarang <br>";
+echo "Harga Barang : $harga";
+ ?>
