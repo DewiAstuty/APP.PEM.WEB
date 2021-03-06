db83b91fdb445f0ec7be9df0d9eeac552d093a30
DewiAstuty_ <79793340+DewiAstuty@users.noreply.github.com>
2021-03-07 00:34:07 +0700

    Add files via upload

---
+++ penugasan.php
@@ -0,0 +1,8 @@
+<?php 
+ $kecepatan=83;
+ //ARITMATIKA
+ $kecepatan = $kecepatan + 10;
+ //PENUGASAN
+ $kecepatan += 10;
+ echo $kecepatan
+ ?>
---
+++ relasi.php
@@ -0,0 +1,14 @@
+<?php 
+ $a=10;
+ $b=5;
+
+ //menggunakan operator relasi lebih besar
+ $c=$a>$b;
+ echo "$a>$b: $c <br>";
+ echo "<hr>";
+
+ //menggunakan operator relasi lebih besar
+ $d=$a<$b;
+ echo "$a<$b: $d <br>";
+ echo "<hr>";
+ ?>
